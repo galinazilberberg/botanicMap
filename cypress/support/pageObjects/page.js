a475aa55abcfebd075e95a14e9e2a0444f4eea90
tanyakars <euphratus.tk@gmail.com>
2021-05-20 20:32:37 -0700

    new fn for main page

--- cypress/support/pageObjects/page.js
+++ cypress/support/pageObjects/page.js
@@ -1,21 +1,45 @@
-export default class Page {
-    linkHeader() {return cy.get('.container')};
-    linkPlant() {return cy.get('//a[contains(text(),\'Plant\')]')};
-    linkHardinessZone() {return cy.get('//a[contains(text(),\'HardinessZone\')]')}
-    linkLogin() {return cy.get('.mr-4')};
-    linkSignUp() {return cy.get('.register-button')};
-    linkContact() {return cy.get('//a[contains(text(),\'Contact\')]')};
-    linkSubscribe() {return cy.get('//a[contains(text(),\'Subscribe\')]')};
-    linkFAQ() {return cy.get('//a[contains(text(),\'FAQ\')]')};
-    linkToS() {return cy.get('//b[contains(text(),\'Terms of Service\')]')};
-    linkPP() {return cy.get('//b[contains(text(),\'Privacy Policy\')]')};
-    dropDownMenu() {return cy.get('.ant-dropdown-link')};
-    header() {return cy.get('.jumbo-title')};
-    BtnGetStarted() {return cy.get('.ant-btn-primary')};
+import milliseconds from "mocha/lib/ms";
 
-    // clearInput(element){
-    //     const value =  cy.element.getValue();
-    //     for(let i = 0; i < value.length; i++){
-    //         await element.keys(['Backspace']);
-    //     }
+export default class Page {
+    linkHeader = () => cy.get('.container');
+    linkPlant = () => cy.get('//a[contains(text(),\'Plant\')]');
+    linkHardinessZone = () => cy.get('//a[contains(text(),\'HardinessZone\')]');
+    linkLogin = () => cy.get('.mr-4');
+    linkSignUp = () => cy.get('.register-button');
+    linkContact = () => cy.get('//a[contains(text(),\'Contact\')]');
+    linkSubscribe = () => cy.get('//a[contains(text(),\'Subscribe\')]');
+    linkFAQ = () => cy.get('//a[contains(text(),\'FAQ\')]');
+    linkToS = () => cy.get('//b[contains(text(),\'Terms of Service\')]');
+    linkPP = () => cy.get('//b[contains(text(),\'Privacy Policy\')]');
+    dropDownMenu = () => cy.get('.ant-dropdown-link');
+    header = () => cy.get('.jumbo-title');
+    btnGetStarted = () => cy.get('.ant-btn-primary');
+
+    navigate() {
+        cy.visit('')
+    };
+
+    elementsBeforeLoginExists() {
+        this.linkHeader().should('be.visible');
+        this.linkLogin().should('be.visible');
+        this.linkSignUp().should('be.visible');
+        this.header().should('be.visible');
+        this.btnGetStarted().should('be.visible');
+    };
+
+    elementsAfterLoginExists() {
+        this.linkHeader().should('be.visible');
+        this.linkPlant().should('be.visible');
+        this.linkHardinessZone().should('be.visible');
+        this.dropDownMenu().should('be.visible');
+    };
+
+    footerExist() {
+        this.linkContact().should('be.visible');
+        this.linkSubscribe().should('be.visible');
+        this.linkFAQ().should('be.visible');
+        this.linkToS().should('be.visible');
+        this.linkPP().should('be.visible');
+    };
+
 }
