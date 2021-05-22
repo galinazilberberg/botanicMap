d099aafcce8bc27868b45b535b1d6f0f078d4278
tanyakars <euphratus.tk@gmail.com>
2021-05-21 18:54:45 -0700

    test data user, amin, invalid data

---
+++ cypress/support/data/test.data.js
@@ -0,0 +1,16 @@
+export const user = {
+    email: 'euphratus.tk@gmail.com',
+    password: 'doc1020',
+    firstName: 'Tanya',
+    SecondName: 'Karsova'
+}
+
+export const admin = {
+    email: 'exampel@mail.com',
+    password: '321321',
+    firstName: 'Admin',
+    secondName: 'Admin'
+}
+
+export const wrongEmail = 'WrongEmail@gmail.com'
+export const wrongPassword = '123456'
