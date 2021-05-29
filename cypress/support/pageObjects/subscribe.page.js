54f66e5a0742c0f704acce44b65d5df67afa1592
tanyakars <euphratus.tk@gmail.com>
2021-05-28 23:13:09 -0700

    new fn for smoke

--- cypress/support/pageObjects/subscribe.page.js
+++ cypress/support/pageObjects/subscribe.page.js
@@ -2,6 +2,9 @@ import Page from "./page";
 
 class SubscribePage extends Page{
     header = () => cy.get('h4');
+    labelEmail = () => cy.get('label[title=\'Email\']');
+    fieldEmail = () => cy.get('#basic_email');
+    btnSubmit = () => cy.get('.login-form-button');
 
     open() {
         cy.visit('/subscribe')
@@ -9,6 +12,24 @@ class SubscribePage extends Page{
 
     isOpen() {
         this.header().should('have.text', 'Subscribe to our Newsletter');
+    };
+
+    elementsExists() {
+        this.header().should('be.visible');
+        this.labelEmail().should('be.visible');
+        this.fieldEmail().should('be.visible');
+        this.btnSubmit().should('be.visible');
+    };
+
+    labelsAreCorrect() {
+        this.header().should('have.text', 'Subscribe to our Newsletter');
+        this.labelEmail().should('have.text', 'Email');
+        this.btnSubmit().should('have.text', 'Submit Inquiry');
+    };
+
+    submitBtnIsEnabled(email) {
+        this.fieldEmail().type(email);
+        this.btnSubmit().should('be.enabled');
     };
 }
 
